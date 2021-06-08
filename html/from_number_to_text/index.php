9d7cfcad672368bb641a7b1e0d66d97567853642
Tranngocanh1911 <83797409+Tranngocanh1911@users.noreply.github.com>
2021-06-08 16:17:31 +0700

    Update index.php

--- html/from_number_to_text/index.php
+++ html/from_number_to_text/index.php
@@ -8,47 +8,73 @@
     <title>Document</title>
 </head>
 <body>
-<form>
+<form method="post">
     <input type="number" name="search" placeholder="Enter your number"/>
-    <input type="submit" id="submit" value="Find"/>
+    <input type="submit" id="submit" value="convert"/>
 </form>
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $searchNumber = $_POST['search'];
-    $number = $searchNumber;
+$arr = [
+    1 => 'one',
+    2 => 'two',
+    3 => 'three',
+    4 => 'four',
+    5 => 'five',
+    6 => 'six',
+    7 => 'seven',
+    8 => 'eight',
+    9 => 'nine',
+    10 => 'ten',
+    11 => 'eleven',
+    12 => 'twelve',
+    13 => 'thirteen',
+    14 => 'fourteen',
+    15 => 'fifteen',
+    16 => 'sixteen',
+    17 => 'seventeen',
+    18 => 'eighteen',
+    19 => 'nineteen',
+    20 => 'twenty',
+    30 => 'thirty',
+    40 => 'forty',
+    50 => 'fifty',
+    60 => 'sixty',
+    70 => 'seventy',
+    80 => 'eighty',
+    90 => 'ninety',
+    100 => 'one hundred',
+    200 => 'two hundred',
+    300 => 'three hundred',
+    400 => 'four hundred',
+    500 => 'five hundred',
+    600 => 'six hundred',
+    700 => 'seven hundred',
+    800 => 'eight hundred',
+    900 => 'nine hundred',
+    1000 => 'one thousand',
+];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $number = $_POST['search'];
     switch ($number) {
-        case '0':
-            echo 'zero';
+        case 0:
+            echo '0-zero';
             break;
-        case '1':
-            echo 'one';
+        case $number<0:
+            echo 'out of ability';
             break;
-        case '2':
-            echo 'two';
+        case $number < 20:
+            $string = $number.'-'.$arr[$number];
+            echo $string;
             break;
-        case '3':
-            echo 'three';
+        case $number <100:
+            $string1 = $number.'-'.$arr[$number-$number%10].' '.$arr[$number%10];
+            echo $string1;
             break;
-        case '4':
-            echo 'four';
-            break;
-        case '5':
-            echo 'five';
-            break;
-        case '6':
-            echo 'six';
-            break;
-        case '7':
-            echo 'seven';
-            break;
-        case '8':
-            echo 'eight';
-            break;
-        case '9':
-            echo 'nine';
+        case $number <=1000:
+            $string2 = $number.'-'.$arr[$number-$number%100].' '.$arr[($number%100)-($number%10)].' '.$arr[($number%100)%10];
+            echo $string2;
             break;
         default:
-            echo 'Out of ability';
+            echo 'out of ability';
     }
 }
 ?>
